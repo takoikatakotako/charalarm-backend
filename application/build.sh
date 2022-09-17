e55927dfc2c26930a847864d4fdc746c2ef9d847
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2022-09-17 11:29:21 +0900

    zipにして書き出すようにした。

--- application/build.sh
+++ application/build.sh
@@ -1,11 +1,19 @@
 # healthcheck
 GOOS=linux GOARCH=amd64 go build -o ./build/healthcheck healthcheck.go
+zip ./build/healthcheck.zip ./build/healthcheck
 
 # signup_anonymous_user
 GOOS=linux GOARCH=amd64 go build -o ./build/signup_anonymous_user signup_anonymous_user.go
+zip ./build/signup_anonymous_user.zip ./build/signup_anonymous_user
 
 # info_anonymous_user
 GOOS=linux GOARCH=amd64 go build -o ./build/info_anonymous_user info_anonymous_user.go
+zip ./build/info_anonymous_user.zip ./build/info_anonymous_user
 
 # withdraw_anonymous_user
 GOOS=linux GOARCH=amd64 go build -o ./build/withdraw_anonymous_user withdraw_anonymous_user.go
+zip ./build/withdraw_anonymous_user.zip ./build/withdraw_anonymous_user
+
+
+# remove not zip file
+cd build && ls | grep -v -E '.zip$' | xargs rm -r
