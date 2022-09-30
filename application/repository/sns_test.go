2bee404cde8d3e733454918a51890b3815b17b31
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2022-09-30 23:40:51 +0900

    エンドポイント重複時のテストを作成

--- application/repository/sns_test.go
+++ application/repository/sns_test.go
@@ -2,7 +2,8 @@ package repository
 
 import (
 	"testing"
-	// "fmt"
+	"fmt"
+	"reflect"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
@@ -15,5 +16,21 @@ func TestCreatePlatformEndpoint(t *testing.T) {
 	if err != nil {
 		t.Errorf("unexpected error: %v", err)
 	}
-	assert.NotEqual(t, len(response.EndpointArn), 0)
+
+	response, err = repository.CreateIOSVoipPlatformEndpoint(token)
+	if err != nil {
+
+	// 	switch t := err.(type) {
+	// 	default:
+	// 		fmt.Println("not a model missing error")
+	//    }
+
+		fmt.Printf("type of a is %v\n", reflect.TypeOf(err))
+
+
+		// fmt.Println(err.(type))
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	assert.NotEqual(t, len(response.EndpointArn), 100)
 }
