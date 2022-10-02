98b48b04819dd46b67884f0686044bc556890b03
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2022-10-03 01:20:10 +0900

    プラットフォームを追加

--- localstack/createPlatformApplication.sh
+++ localstack/createPlatformApplication.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x
 
+# iOS Push
+awslocal sns create-platform-application \
+    --name ios-push-platform-application \
+    --platform APNS \
+    --attributes PlatformCredential=DAMMY
+
+# iOS VoIP Push
 awslocal sns create-platform-application \
     --name ios-voip-push-platform-application \
     --platform APNS \
