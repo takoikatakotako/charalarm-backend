126f2ce1d6da90aee4ede16766cf5c4b970838c1
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2022-10-11 23:31:41 +0900

    Batch処理を追加

--- application/service/batch.go
+++ application/service/batch.go
@@ -21,6 +21,9 @@ func (b *BatchService) QueryDynamoDBAndSendMessage(hour int, minute int, weekday
 		return err
 	}
 
+	// 何回もDynamoDBにアクセスすると結構大変だからメモ化する
+	// メモしてメモする
+
 	// ランダム再生用のキャラクターのボイスを取得
 	randomChara, err := b.DynamoDBRepository.GetRandomChara()
 	if err != nil {
@@ -28,10 +31,15 @@ func (b *BatchService) QueryDynamoDBAndSendMessage(hour int, minute int, weekday
 	}
 	callVoicesCount := len(randomChara.CharaCall.Voices)
 	if callVoicesCount == 0 {
+		// TODO. エラーを収集する仕組みを追加
 		// エラーだよ
 	}
 	index := rand.Intn(callVoicesCount)
 	randomVoice := chara.CharaCall.Voices[index]
+
+	// ランダム用のメモを作成
+	randomCharaVoiceMap := map[string]string{}
+	randomCharaVoiceMap["RANDOM"] = randomVoice
 
 	// AlarmInfoに変換してSQSに送信
 	for _, alarm := range alarmList {
@@ -40,9 +48,22 @@ func (b *BatchService) QueryDynamoDBAndSendMessage(hour int, minute int, weekday
 		alarmInfo.AlarmID = alarm.AlarmID
 		alarmInfo.UserID = alarm.UserID
 
+		// randomCharaVoiceMap にキーがあるか確認する
+		if val, ok := dict[alarm.CharaID]; ok {
+			// キーある場合
+			alarmInfo.CharaName = "xxx"
+			alarmInfo.FileURL = "xxxx"	
+		} else {
+			// キーがないのでDynamoDBから取得するよ
+
+			// メモ化ようのアレにも登録するよ
+
+			// エラーが出たらログ出してcontinue
+		}
+
+
 		alarmInfo.SNSEndpointArn = "xxx"
-		alarmInfo.CharaName = "xxx"
-		alarmInfo.FileURL = randomVoice
+
 
 		// SQSに送信
 		err := b.SQSRepository.SendAlarmInfoMessage(alarmInfo)
