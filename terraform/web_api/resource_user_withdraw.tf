c78615ce9ea81bed2ee0698742f5795e2623a77c
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-02-04 13:41:32 +0900

    anoymous_user の回想を削除

--- terraform/web_api/resource_user_withdraw.tf
+++ terraform/web_api/resource_user_withdraw.tf
@@ -6,3 +6,36 @@ resource "aws_api_gateway_resource" "user_withdraw_resource" {
   parent_id   = aws_api_gateway_resource.user_resource.id
   path_part   = "withdraw"
 }
+
+##################################################
+# Lambda
+##################################################
+module "user_withdraw_post_lambda_function" {
+  source        = "./lambda_function"
+  function_name = "user-withdraw-post-function"
+  role          = aws_iam_role.api_gateway_lambda_role.arn
+  handler       = "user_withdraw_user"
+  s3_bucket     = local.application_bucket_s3_url
+  s3_key        = "/${var.application_version}/user_withdraw.zip"
+  execution_arn = aws_api_gateway_rest_api.charalarm_rest_api.execution_arn
+  method        = "POST"
+  path          = "/user/withdraw"
+}
+
+##################################################
+# Method
+##################################################
+module "user_withdraw_post_method" {
+  source      = "./method"
+  rest_api_id = aws_api_gateway_rest_api.charalarm_rest_api.id
+  resource_id = aws_api_gateway_resource.user_withdraw_resource.id
+  http_method = "POST"
+  lambda_uri  = module.user_withdraw_post_lambda_function.invoke_arn
+}
+
+module "user_withdraw_options_method" {
+  source      = "./options_method"
+  rest_api_id = aws_api_gateway_rest_api.charalarm_rest_api.id
+  resource_id = aws_api_gateway_resource.user_withdraw_resource.id
+}
+
