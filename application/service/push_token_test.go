0b325588dfdc229fce3a158d9e92311ac038a467
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-04-25 23:30:27 +0900

    テストを追加

--- application/service/push_token_test.go
+++ application/service/push_token_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/google/uuid"
@@ -43,7 +44,7 @@ func TestPushTokenService_AddIOSPushToken(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, getUser.IOSVoIPPushToken.Token, pushToken)
-	assert.NotEqual(t, getUser.IOSVoIPPushToken.SNSEndpointArn, "")
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
 }
 
 // iOSPushTokenを変更できる
@@ -82,7 +83,7 @@ func TestPushTokenService_AddIOSPushTokenCanChange(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, getUser.IOSVoIPPushToken.Token, oldPushToken)
-	assert.NotEqual(t, getUser.IOSVoIPPushToken.SNSEndpointArn, "")
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
 
 	// トークン更新
 	err = pushService.AddIOSVoipPushToken(userID, authToken, newPushToken)
@@ -98,7 +99,50 @@ func TestPushTokenService_AddIOSPushTokenCanChange(t *testing.T) {
 
 	// Assert
 	assert.Equal(t, getUser.IOSVoIPPushToken.Token, newPushToken)
-	assert.NotEqual(t, getUser.IOSVoIPPushToken.SNSEndpointArn, "")
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
+}
+
+// iOSPushTokenを複数回更新できる
+func TestPushTokenService_AddIOSPushTokenMultiTimes(t *testing.T) {
+	// Repository
+	dynamoDBRepository := repository.DynamoDBRepository{IsLocal: true}
+	snsRepository := repository.SNSRepository{IsLocal: true}
+
+	// Service
+	userService := UserService{Repository: dynamoDBRepository}
+	pushService := PushTokenService{DynamoDBRepository: dynamoDBRepository, SNSRepository: snsRepository}
+
+	// ユーザー作成
+	userID := uuid.New().String()
+	authToken := uuid.New().String()
+	ipAddress := "127.0.0.1"
+	pushToken := uuid.New().String()
+
+	err := userService.Signup(userID, authToken, ipAddress)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// トークン作成
+	err = pushService.AddIOSVoipPushToken(userID, authToken, pushToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	err = pushService.AddIOSVoipPushToken(userID, authToken, pushToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// ユーザー取得
+	getUser, err := userService.GetUser(userID, authToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// Assert
+	assert.Equal(t, getUser.IOSVoIPPushToken.Token, pushToken)
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
 }
 
 // iOSVoIPPushTokenを登録できる
@@ -137,4 +181,102 @@ func TestPushTokenService_AddIOSVoipPushToken(t *testing.T) {
 	// Assert
 	assert.Equal(t, getUser.IOSVoIPPushToken.Token, pushToken)
 	assert.NotEqual(t, getUser.IOSVoIPPushToken.SNSEndpointArn, "")
+}
+
+// iOSVoIPPushTokenを変更できる
+func TestPushTokenService_AddIOSVoipPushTokenCanChange(t *testing.T) {
+	// Repository
+	dynamoDBRepository := repository.DynamoDBRepository{IsLocal: true}
+	snsRepository := repository.SNSRepository{IsLocal: true}
+
+	// Service
+	userService := UserService{Repository: dynamoDBRepository}
+	pushService := PushTokenService{DynamoDBRepository: dynamoDBRepository, SNSRepository: snsRepository}
+
+	// ユーザー作成
+	userID := uuid.New().String()
+	authToken := uuid.New().String()
+	ipAddress := "127.0.0.1"
+	oldPushToken := uuid.New().String()
+	newPushToken := uuid.New().String()
+
+	err := userService.Signup(userID, authToken, ipAddress)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// トークン作成
+	err = pushService.AddIOSVoipPushToken(userID, authToken, oldPushToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// ユーザー取得
+	getUser, err := userService.GetUser(userID, authToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// Assert
+	assert.Equal(t, getUser.IOSVoIPPushToken.Token, oldPushToken)
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
+
+	// トークン更新
+	err = pushService.AddIOSVoipPushToken(userID, authToken, newPushToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// ユーザー取得
+	getUser, err = userService.GetUser(userID, authToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// Assert
+	assert.Equal(t, getUser.IOSVoIPPushToken.Token, newPushToken)
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
+}
+
+// iOSVoIPPushTokenを複数回変更できる
+func TestPushTokenService_AddIOSVoipPushTokenMultiChange(t *testing.T) {
+	// Repository
+	dynamoDBRepository := repository.DynamoDBRepository{IsLocal: true}
+	snsRepository := repository.SNSRepository{IsLocal: true}
+
+	// Service
+	userService := UserService{Repository: dynamoDBRepository}
+	pushService := PushTokenService{DynamoDBRepository: dynamoDBRepository, SNSRepository: snsRepository}
+
+	// ユーザー作成
+	userID := uuid.New().String()
+	authToken := uuid.New().String()
+	ipAddress := "127.0.0.1"
+	pushToken := uuid.New().String()
+
+	err := userService.Signup(userID, authToken, ipAddress)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// トークン作成
+	err = pushService.AddIOSVoipPushToken(userID, authToken, pushToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	err = pushService.AddIOSVoipPushToken(userID, authToken, pushToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// ユーザー取得
+	getUser, err := userService.GetUser(userID, authToken)
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	// Assert
+	assert.Equal(t, getUser.IOSVoIPPushToken.Token, pushToken)
+	assert.True(t, strings.Contains(getUser.IOSVoIPPushToken.SNSEndpointArn, "arn:aws:sns:ap-northeast-1"))
 }
