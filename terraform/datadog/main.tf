34ad8aa56e5530dea94cac4ecad5c60352084348
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-06-03 22:20:52 +0900

    ログをDatadog

--- terraform/datadog/main.tf
+++ terraform/datadog/main.tf
@@ -11,17 +11,13 @@ resource "aws_secretsmanager_secret_version" "dd_api_key" {
   secret_string = var.dd_api_key
 }
 
-#output "dd_api_key" {
-#  value = aws_secretsmanager_secret.dd_api_key.arn
-#}
-
 # Use the Datadog Forwarder to ship logs from S3 and CloudWatch, as well as observability data from Lambda functions to Datadog. For more information, see https://github.com/DataDog/datadog-serverless-functions/tree/master/aws/logs_monitoring
 resource "aws_cloudformation_stack" "datadog_forwarder" {
   name         = "datadog-forwarder"
   capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM", "CAPABILITY_AUTO_EXPAND"]
   parameters = {
     DdApiKeySecretArn = aws_secretsmanager_secret.dd_api_key.arn,
-    DdSite            = "app.datadoghq.com",
+    DdSite            = "datadoghq.com",
     FunctionName      = "datadog-forwarder"
   }
   template_url = "https://datadog-cloudformation-template.s3.amazonaws.com/aws/forwarder/latest.yaml"
