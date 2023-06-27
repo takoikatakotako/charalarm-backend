a59594926c4481ad794fd1b510b71fbf43c6eb64
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-06-27 23:54:08 +0900

    フィンガープリントをしゅうせ

--- terraform/github/main.tf
+++ terraform/github/main.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
 }
 
 # see: https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_create_oidc_verify-thumbprint.html
