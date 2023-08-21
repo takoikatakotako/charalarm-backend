1d5d410b0b2250d9ce1b21784cac3761c3540d37
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-08-21 22:16:22 +0900

    ハンドラー名を修正

--- terraform/web_api/resource_user_update_premium.tf
+++ terraform/web_api/resource_user_update_premium.tf
@@ -14,7 +14,7 @@ module "user_update_premium_lambda_function" {
   source                    = "./lambda_function"
   function_name             = "user-update-premium-post-function"
   role                      = aws_iam_role.api_gateway_lambda_role.arn
-  handler                   = "user-update-premium"
+  handler                   = "user_update_premium"
   s3_bucket                 = local.application_bucket_s3_url
   s3_key                    = "/${var.application_version}/user_update_premium.zip"
   execution_arn             = aws_api_gateway_rest_api.charalarm_rest_api.execution_arn
