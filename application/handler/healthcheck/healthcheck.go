300d69fece1f71d34eafcbccd0640bfab1a1fb33
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-10-29 15:37:48 +0900

    ログを追加

--- application/handler/healthcheck/healthcheck.go
+++ application/handler/healthcheck/healthcheck.go
@@ -3,26 +3,16 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"github.com/takoikatakotako/charalarm-backend/entity/response"
-	"github.com/takoikatakotako/charalarm-backend/util/message"
-	"log/slog"
-	"net/http"
-	"os"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/takoikatakotako/charalarm-backend/entity/response"
+	"github.com/takoikatakotako/charalarm-backend/util/message"
+	"net/http"
 )
 
 func Handler(ctx context.Context, name events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	res := response.MessageResponse{Message: message.Healthy}
 	jsonBytes, _ := json.Marshal(res)
-
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
-	logger.Info("hello structured log", "name", "blue", "No", 5)
-
-	logger.Error("This is Error!!")
-	logger.Warn("This is Warn!")
-	logger.Info("This is Info!")
 
 	return events.APIGatewayProxyResponse{
 		Body:       string(jsonBytes),
