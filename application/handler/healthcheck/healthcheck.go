9f1527744dd756a87c138ffbbb02349874eff312
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2023-10-29 12:36:07 +0900

    slogでエラガーがどうなるか確認

--- application/handler/healthcheck/healthcheck.go
+++ application/handler/healthcheck/healthcheck.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"github.com/takoikatakotako/charalarm-backend/entity/response"
 	"github.com/takoikatakotako/charalarm-backend/util/message"
+	"log/slog"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -14,6 +15,10 @@ import (
 func Handler(ctx context.Context, name events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	res := response.MessageResponse{Message: message.Healthy}
 	jsonBytes, _ := json.Marshal(res)
+
+	slog.Error("This is Error!!")
+	slog.Warn("This is Warn!")
+	slog.Info("This is Info!")
 
 	return events.APIGatewayProxyResponse{
 		Body:       string(jsonBytes),
